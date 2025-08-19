b22af63e5e3a004ba202f7a2f3eab72c5bb1ff1f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 11:41:12 -0300

    Continuação

--- src/main/java/br/fiap/conexao/Teste.java
+++ src/main/java/br/fiap/conexao/Teste.java
@@ -3,11 +3,18 @@ package br.fiap.conexao;
 import br.fiap.modelo.Categoria;
 import br.fiap.modelo.CategoriaDAO;
 
+import java.util.List;
+
 public class Teste {
     public static void main(String[] args) {
-        Categoria categoria = new Categoria(5L,"mais");
+
         CategoriaDAO dao = new CategoriaDAO();
-        dao.inserir(categoria);
+
+
+        List<Categoria> lista = dao.listar();
+        for (Categoria categoria : lista) {
+            System.out.println(categoria.getId() + "-->" + categoria.getCategoria());
+        }
     }//main
 
 }//class
--- src/main/java/br/fiap/modelo/CategoriaDAO.java
+++ src/main/java/br/fiap/modelo/CategoriaDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriaDAO {
     private PreparedStatement ps;
@@ -24,7 +26,7 @@ public class CategoriaDAO {
     public void inserir(Categoria categoria) {
         String sql = "insert into java_categoria values(?, ?)";
 
-      try {
+      try (Connection connection = Conexao.conectar()) {
             ps = connection.prepareStatement(sql);
             ps.setLong(1,categoria.getId());
             ps.setString(2,categoria.getCategoria());
@@ -35,5 +37,20 @@ public class CategoriaDAO {
         }
     }//inserir
 
+public List<Categoria> listar() {
+        List<Categoria> lista = new ArrayList<>();
+        String sql = "select * from java_categoria";
 
+    try (Connection connection = Conexao.conectar()) {
+        ps = connection.prepareStatement(sql);
+        rs = ps.executeQuery();
+        while (rs.next()){
+            lista.add(new Categoria(rs.getLong("id_categoria"), rs.getString("categoria")));        }
+    }
+    catch(SQLException e){
+        System.out.println("Erro ao listar categorias\n"+ e);
+    }
+
+        return lista;
+}//listar
 }//class
